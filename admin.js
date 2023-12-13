aeea91793d14206c96f1203fd08c0cd6fba0ad9b
Shubham Kumar <shubham.kumar.min21@itbhu.ac.in>
2023-12-13 10:01:46 +0530

    add more

--- admin.js
+++ admin.js
@@ -1,21 +1,24 @@
 const { kafka } = require("./client");
+
 async function init() {
   const admin = kafka.admin();
-  console.log("Admin Connecting........")
-  await admin.connect();
-  console.log("Admin Connected!")
-  console.log("Creating Topic");
+  console.log("Admin connecting...");
+  admin.connect();
+  console.log("Adming Connection Success...");
+
+  console.log("Creating Topic [rider-updates]");
   await admin.createTopics({
     topics: [
       {
-        topic: "rider-updated",
-        numPartitions: 2
+        topic: "rider-updates",
+        numPartitions: 2,
       },
     ],
   });
-  console.log("Topic Created");
-  console.log("Admin Disconnecting");
+  console.log("Topic Created Success [rider-updates]");
+
+  console.log("Disconnecting Admin..");
   await admin.disconnect();
-  console.log("Admin Disconnected");
 }
-init()
+
+init();
--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const { Kafka } = require("kafkajs");
 require("dotenv").config()
+console.log(`${process.env.MYIP}:9092`)
 exports.kafka = new Kafka({
   clientId: "my-app",
-  brokers: [`http://${process.env.MYIP}:9092`],
+  brokers: [`${process.env.MYIP}:9092`],
 });
