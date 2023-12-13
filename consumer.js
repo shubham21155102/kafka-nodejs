f427a6ff76be34c16b7c194c14fd059233790fea
Shubham Kumar <shubham.kumar.min21@itbhu.ac.in>
2023-12-13 10:03:35 +0530

    add more

--- consumer.js
+++ consumer.js
@@ -1,15 +1,20 @@
-const {kafka}=require("./client")
-async function init(){
-    const consumer=kafka.consumer({groupId:"test"})
-    console.log("Consumer Connecting........")
-    await consumer.connect()
-    console.log("Consumer Connected!")
-    await consumer.subscribe({topic:"rider-updates"})
-    await consumer.run({
-        eachMessage:async result=>{
-            console.log("Received Message")
-            console.log(result)
-        }
-    })
+const { kafka } = require("./client");
+const group = process.argv[2];
+
+async function init() {
+  const consumer = kafka.consumer({ groupId: group });
+  await consumer.connect();
+
+  await consumer.subscribe({ topics: ["rider-updates"], fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
+      console.log(
+        `${group}: [${topic}]: PART:${partition}:`,
+        message.value.toString()
+      );
+    },
+  });
 }
-init()
+
+init();
--- producer.js
+++ producer.js
@@ -1,25 +1,36 @@
-const {kafka} =require("./client")
-async function init(){
-    const producer = kafka.producer()
-    console.log("Producer Connecting........")
-    await producer.connect()
-    console.log("Producer Connected!")
-    console.log("Sending Message")
+const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function init() {
+  const producer = kafka.producer();
+
+  console.log("Connecting Producer");
+  await producer.connect();
+  console.log("Producer Connected Successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
     await producer.send({
-        topic:"rider-updates",
-        messages:[
-            {
-                key:"location-update",
-                value:JSON.stringify({
-                    name:"Tonny Kakkar",
-                    location:"Mumbai"
-                })
-            }
-        ]
-    })
-    console.log("Message Sent")
-    console.log("Producer Disconnecting")
-    await producer.disconnect()
-    console.log("Producer Disconnected")
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
+  });
 }
-init()
+
+init();
